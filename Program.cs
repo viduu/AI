354d0572539e8762403db309128dc83dc40ba764
viduu <52102657+viduu@users.noreply.github.com>
2026-07-30 01:32:16 +0530

    Initialize ASP.NET Core application with Swagger

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure PostgreSQL Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
